e79f609d09be07d29d9ca1494e1e363ca32d40e1
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-09 11:23:24 +0100

    Add SeedData To Entities

--- Company.Data/Contexts/CompanyContext.cs
+++ Company.Data/Contexts/CompanyContext.cs
@@ -19,6 +19,129 @@ public class CompanyContext : DbContext
     {
         base.OnModelCreating(builder);
         builder.Entity<EmployeesJobTitles>().HasKey(ejt => new { ejt.EmployeeId, ejt.JobTitleId});
+        SeedData(builder);
+    }
+
+    private void SeedData(ModelBuilder builder)
+    {
+        var Company = new List<Companies>
+        {
+            new Companies
+            {
+                Id= 1,
+                Name="Pendant publishing",
+                Organisationsnummer="550 6600"
+            },
+            new Companies
+            {
+                Id= 2,
+                Name="Weyland-Yutani",
+                Organisationsnummer="551 6622"
+            }
+
+        };
+        builder.Entity<Companies>().HasData(Company);
+        var Department = new List<Departments>
+        {
+            new Departments
+            {
+                Id= 1,
+                DepartmentName="Shipbuilding",
+                CompanyId=2
+            },
+            new Departments
+            {
+                Id= 2,
+                DepartmentName="Editorial Department",
+                CompanyId=1
+            }
+        };
+        builder.Entity<Departments>().HasData(Department);
+        var Employee = new List<Employees>
+        {
+            new Employees
+            {
+                Id= 1,
+                FirstName="Ellen",
+                LastName="Ripley",
+                Unionized=false,
+                Salary=1000,
+                DepartmentId=1
+            },
+            new Employees
+            {
+                Id= 2,
+                FirstName="Elaine",
+                LastName="Benes",
+                Unionized=false,
+                Salary=10000,
+                DepartmentId=2
+            },
+            new Employees
+            {
+                Id=3,
+                FirstName="Dwayne",
+                LastName="Hicks",
+                Unionized=true,
+                Salary=100,
+                DepartmentId=1
+            }
+
+    };
+        builder.Entity<Employees>().HasData(Employee);
+        var JobTitle = new List<JobTitles>
+        {
+            new JobTitles
+            {
+                Id= 1,
+                Title="Corporal",
+            },
+            new JobTitles
+            {
+                Id= 2,
+                Title="Warrant officer"
+            },
+            new JobTitles
+            {
+                Id= 3,
+                Title="Crewmember"
+            },
+            new JobTitles
+            {
+                Id=4,
+                Title="Copy Editor"
+            }
+        };
+        builder.Entity<JobTitles>().HasData(JobTitle);
+        var EmployeeJobTitle = new List<EmployeesJobTitles>
+        {
+            new EmployeesJobTitles
+            {
+                JobTitleId= 1,
+                EmployeeId= 3,
+            },
+            new EmployeesJobTitles 
+            {
+                JobTitleId= 3,
+                EmployeeId= 3,
+            },
+            new EmployeesJobTitles
+            {
+                JobTitleId= 2,
+                EmployeeId= 1,
+            },
+            new EmployeesJobTitles
+            {
+                JobTitleId= 3,
+                EmployeeId= 1,
+            },
+            new EmployeesJobTitles
+            {
+                JobTitleId= 4,
+                EmployeeId= 2,
+            },
+        };
+        builder.Entity<EmployeesJobTitles>().HasData(EmployeeJobTitle);
     }
 
 }
