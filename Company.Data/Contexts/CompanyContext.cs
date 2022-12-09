443a4929e035102df4aa2db22b00e84c96a057a9
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-09 10:21:32 +0100

    Added Entity Classes to Course Context

--- Company.Data/Contexts/CompanyContext.cs
+++ Company.Data/Contexts/CompanyContext.cs
@@ -5,14 +5,20 @@ namespace Company.Data.Contexts;
 
 public class CompanyContext : DbContext
 {
-	public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
+    public DbSet<Companies> Company => Set<Companies>();
+    public DbSet<Departments> Department => Set<Departments>();
+    public DbSet<Employees> Employee => Set<Employees>();
+    public DbSet<EmployeesJobTitles> EmployeeJobTitle => Set<EmployeesJobTitles>();
+    public DbSet<JobTitles> JobTitle => Set<JobTitles>();
+
+    public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
 	{
 
 	}
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        builder.Entity<EmployeeJobTitles>().HasKey(ejt => new { ejt.EmployeeId, ejt.JobTitleId});
+        builder.Entity<EmployeesJobTitles>().HasKey(ejt => new { ejt.EmployeeId, ejt.JobTitleId});
     }
 
 }
