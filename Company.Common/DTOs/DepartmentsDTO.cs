a914655ffbfd6a0a0c9073950c95d22beed50203
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-11 21:30:12 +0100

    Add DTOs

---
+++ Company.Common/DTOs/DepartmentsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Common.DTOs;
+
+public record DepartmentsDTO
+{
+    public int Id { get; set; }
+    public string? DepartmentName { get; set; }
+}
---
+++ Company.Common/DTOs/EmployeesJobTitlesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Common.DTOs;
+
+public record EmployeesJobTitlesDTO
+{
+    public int JobTitleId { get; set; }
+    public int EmployeeId { get; set; }
+
+    public EmployeesJobTitlesDTO (int jobTitleId, int employeeId)
+    {
+        JobTitleId = jobTitleId;
+        EmployeeId = employeeId;
+    }
+}
