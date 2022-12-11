16006092062f74652e48a016bdac461db9600904
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-11 22:05:46 +0100

    Add the Service Interface and Class

---
+++ Company.Data/Services/DbService.cs
@@ -0,0 +1,17 @@
+﻿using Company.Data.Interfaces;
+using Company.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Company.Data.Services;
+
+public class DbService : IDbService
+{
+    private readonly CompanyContext _db;
+    private readonly IMapper _mapper;
+    public DbService(CompanyContext db, IMapper mapper) => (_db, _mapper) = (db, mapper);
+}
