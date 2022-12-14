f9da1c3655eef8e120ef3a8f3103646985c95115
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-14 13:53:21 +0100

    Register IdbService and automapper

--- Company.API/Program.cs
+++ Company.API/Program.cs
@@ -1,3 +1,8 @@
+using Company.Common.DTOs;
+using Company.Data.Entities;
+using Company.Data.Services;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+ConfigureAutomapper(builder.Services);
+RegisterServices(builder.Services);
+
 
 var app = builder.Build();
 
@@ -24,3 +32,21 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void ConfigureAutomapper(IServiceCollection services)
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<Companies, CompaniesDTO>().ReverseMap();
+        cfg.CreateMap<Departments, DepartmentsDTO>().ReverseMap();
+        cfg.CreateMap<Employees, EmployeesDTO>().ReverseMap();
+        cfg.CreateMap<JobTitles, JobTitlesDTO>().ReverseMap();
+        cfg.CreateMap<EmployeesJobTitles, EmployeesJobTitlesDTO>().ReverseMap();
+    });
+    var mapper = config.CreateMapper();
+    services.AddSingleton(mapper);
+}
+void RegisterServices (IServiceCollection services)
+{
+    services.AddScoped<IDbService, DbService>();
+}
