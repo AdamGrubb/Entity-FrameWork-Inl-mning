3e5dda290418d34db70fa3e37363534dd4c4c74f
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-14 15:24:36 +0100

    Add another global using statement

--- Company.API/Using.cs
+++ Company.API/Using.cs
@@ -3,8 +3,8 @@ global using AutoMapper;
 global using Company.Data.Contexts;
 global using Company.Data.Entities;
 global using Company.Data.Interfaces;
-//global using Company.Data.Services;
-//global using Company.Common.DTOs;
+global using Company.Data.Services;
+global using Company.Common.DTOs;
 //global using Company.API.Extensions;
 
 namespace Company.API;
