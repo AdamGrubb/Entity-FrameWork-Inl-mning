482c60ce00716016cf9cda729b26d8dff5d08b18
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-15 18:43:00 +0100

    Add controller for EmployeeJobTitle table

---
+++ Company.API/Controllers/EmployeesJobTitlesController.cs
@@ -0,0 +1,37 @@
+﻿using Company.Data.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesJobTitlesController : ControllerBase
+    {
+
+        private readonly IDbService _db;
+        public EmployeesJobTitlesController(IDbService db) => _db = db;
+
+        [HttpPost]
+        public async Task<IResult> Post([FromBody] EmployeesJobTitlesDTO dto)
+        {
+            try
+            {
+                //var entity = await _db.AddAsync<Companies, CompaniesDTO>(dto);
+                var referenceEntity = await _db.AddRefAsync<EmployeesJobTitles, EmployeesJobTitlesDTO>(dto);
+                if (await _db.SaveChangesAsync())
+                {
+                    var node = typeof(EmployeesJobTitles).Name.ToLower();
+                    return Results.Created($"/{node}s/{referenceEntity.JobTitleId}, {referenceEntity.EmployeeId}", referenceEntity); //Fråga jonas om den ska se ut såhär??
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest($"Failed to add {typeof(EmployeesJobTitles).Name} entity. {ex}");
+
+            }
+            return Results.BadRequest($"Failed to add {typeof(EmployeesJobTitles).Name} entity.");
+        }
+    }
+}
