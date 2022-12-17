d48cdb9acf6097b2047f5fbcbed88682ff757de7
Adam Grubb <adam.grubb.63@hotmail.com>
2022-12-17 15:01:46 +0100

    Make a new migration to make Id not nullable

---
+++ Company.Data/Migrations/20221217135019_FixedBugWithExtension.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Company.Data.Migrations
+{
+    public partial class FixedBugWithExtension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Company_CompanyId",
+                table: "Department");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyId",
+                table: "Department",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Company_CompanyId",
+                table: "Department",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Company_CompanyId",
+                table: "Department");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyId",
+                table: "Department",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Company_CompanyId",
+                table: "Department",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id");
+        }
+    }
+}
